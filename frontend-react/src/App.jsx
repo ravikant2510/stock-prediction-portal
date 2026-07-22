b8742e9354ec87f5bb45ab20f913bfc32086c0be
Ravi Kant <purbeyravi123@gmail.com>
2026-07-22 19:43:32 +0530

    homepage react

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,9 +1,14 @@
 import './App.css'
-
+import './assets/css/style.css'
+import Header from './components/Header'
+import Main from './components/Main'
+import Footer from './components/Footer'
 function App() {
   return (
     <>
-      {/* Main App component */}
+      <Header/>
+      <Main/>
+      <Footer/>
     </>
   )
 }

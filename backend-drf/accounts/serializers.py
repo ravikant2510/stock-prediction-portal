540b44d271935574085b3ae21f8096e2edb4abd1
Ravi Kant <purbeyravi123@gmail.com>
2026-07-24 19:58:50 +0530

    registration backend/api

---
+++ backend-drf/accounts/serializers.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True,style={'input_type': 'password'})
+    class Meta:
+        model = User
+        fields = ['username','email','password']
+
+    def create(self,validated_data):
+        # user.objects.create = save the password in a plain text
+        # User.objects.create_user = automatically hash the password
+
+        user = User.objects.create_user(
+            validated_data['username'],
+            validated_data['email'],
+            validated_data['password']
+        )
+        # user = User.objects.create_user(**validated_data)
+        return user
